1e4b4707a2c68fa2262873f61173e7488fb000df
Ilia Groshev <groshev.i.d@gmail.com>
2025-06-24 15:45:23 +0100

    start to implement locked balance feat

--- src/Token.sol
+++ src/Token.sol
@@ -22,8 +22,23 @@ contract Token is
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant WITHDRAW_ADMIN_ROLE = keccak256("WITHDRAW_ADMIN_ROLE");
 
+    /// @custom:storage-layout erc7201:namespace=locked.balances
+    struct TokenStorage {
+        mapping(address => uint256) balances;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable state-variable-assignment
     /// @custom:oz-upgrades-unsafe-allow constructor
+    bytes32 private constant _storageLocation = 0x328bcd7529501e9742d6c12f5f2712cc411df6c15a942f4232ff0ece51406d34; // keccak256("erc7201:namespace=locked.balances");
+
+    function _getLockedBalancesStorage() private pure returns (TokenStorage storage $) {
+        assembly {
+            $.slot := _storageLocation
+        }
+    }
+
     constructor() {
         _disableInitializers();
     }
@@ -53,5 +68,35 @@ contract Token is
 
     function burnFrom(address account, uint256 value) public override onlyRole(BURNER_ROLE) {
         super.burnFrom(account, value);
+    }
+
+    function lock(address account, uint256 amount) public onlyRole(WITHDRAW_ADMIN_ROLE) {
+        _getLockedBalancesStorage().balances[account] += amount;
+    }
+
+    function unlock(address account, uint256 amount) public onlyRole(WITHDRAW_ADMIN_ROLE) {
+        _getLockedBalancesStorage().balances[account] -= amount;
+    }
+
+    function _update(address from, address to, uint256 value) internal override {
+        super._update(from, to, value);
+        if (msg.sig == this.withdrawLockedBalance.selector) {
+            return;
+        }
+
+        uint256 lockedBalance = _getLockedBalancesStorage().balances[from];
+        uint256 currentBalance = balanceOf(from);
+        if (currentBalance < lockedBalance) {
+            revert("Transfer would exceed locked balance");
+        }
+    }
+
+    function withdrawLockedBalance(address account, uint256 amount) public onlyRole(WITHDRAW_ADMIN_ROLE) {
+        uint256 lockedBalance = _getLockedBalancesStorage().balances[account];
+        if (amount > lockedBalance) {
+            revert("Insufficient locked balance");
+        }
+        // _getLockedBalancesStorage().balances[account] = lockedBalance - amount;
+        _transfer(account, msg.sender, amount);
     }
 }
